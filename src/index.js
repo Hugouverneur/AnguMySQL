3d1be4c8c5ff3f4202bc3a4a5b3c0924c5910e47
Hugo <hugo.umbert@estiam.com>
2021-06-08 10:29:09 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const users = require('./users');
+
+// Connexion à la base de donnée
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'testangumysql'
+});
+
+connection.connect();
+
+const port = process.env.PORT || 8080;
+
+const app = express()
+    .use(cors())
+    .use(bodyParser.json())
+    .use(users(connection));
+
+app.listen(port, () => {
+    console.log(`Express server listening on port ${port}`);
+});
